55621871c60a25fbbaf0bf1271159eb4280faab8
Donutask <51319000+Donutask@users.noreply.github.com>
2024-08-13 16:18:30 +1000

    Change results shown. Add comments

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+// Refrences to HTML elements
 const resultDisplay = document.getElementById("result");
 const detailsDisplay = document.getElementById("details");
 
+// Don't need too much precision for weather, caching should be fine too
 const locationOptions = {
     enableHighAccuracy: false,
     timeout: 5000,
-    maximumAge: 60000, // One min cache
+    maximumAge: 60000,
 };
 
+// Got users location, now make API request.
+// Using https://open-meteo.com/
+// License: https://creativecommons.org/licenses/by/4.0/
 function LocationSuccess(pos) {
     const crd = pos.coords;
-
     const url = `https://api.open-meteo.com/v1/forecast?latitude=${crd.latitude}&longitude=${crd.longitude}&daily=precipitation_probability_max&forecast_days=1`
 
     detailsDisplay.innerHTML = "Getting weather";
@@ -29,9 +33,9 @@ function LocationSuccess(pos) {
         .catch(error => {
             DisplayError(error);
         });
-
 }
 
+// Use GeolocationError codes and make them human readable (kinda)
 function LocationError(err) {
     resultDisplay.className = "";
 
@@ -44,56 +48,61 @@ function LocationError(err) {
     }
 }
 
+// Get probability of precipitation from weather api result. Show on an informal yeah/nah scale and also show the percentages.
 function DisplayWeatherResult(data) {
     const p = data["daily"]["precipitation_probability_max"][0];
-    console.log(data);
 
     resultDisplay.className = "";
 
-    if (isNaN(p)) {
+    // Weird result: ???
+    // 0 - 5: No!
+    // 5 - 20: No
+    // 20 - 30: Nah
+    // 30 - 70: Maybe
+    // 70 - 80: Yeah
+    // 80 - 95: Yes
+    // 95 - 100: Yes!
+    if (isNaN(p) || !isFinite(p)) {
         resultDisplay.innerHTML = "???"
-    } else if (!isFinite(p)) {
-        resultDisplay.innerHTML = "YES!!!";
-    } else if (p <= 0) {
-        resultDisplay.innerHTML = "Not At All";
-    } else if (p < 10) {
+    } else if (p <= 5) {
+        resultDisplay.innerHTML = "No!";
+    } else if (p <= 20) {
         resultDisplay.innerHTML = "No"
-    } else if (p < 40) {
-        resultDisplay.innerHTML = "Probably Not";
-    } else if (p < 50) {
+    } else if (p <= 30) {
+        resultDisplay.innerHTML = "Nah"
+    } else if (p <= 70) {
         resultDisplay.innerHTML = "Maybe";
-    } else if (p < 75) {
-        resultDisplay.innerHTML = "Probably";
-    } else if (p < 80) {
-        resultDisplay.innerHTML = "Quite Likely";
-    } else if (p < 90) {
-        resultDisplay.innerHTML = "Very Likely";
-    } else if (p < 100) {
+    } else if (p <= 80) {
+        resultDisplay.innerHTML = "Yeah";
+    } else if (p < 95) {
         resultDisplay.innerHTML = "Yes";
-    } else if (p >= 100) {
-        resultDisplay.innerHTML = "Definitely";
+    } else if (p <= 100) {
+        resultDisplay.innerHTML = "Yes!";
+    } else {
+        //Over 100% somehow
+        resultDisplay.innerHTML = "Yes!!";
     }
     detailsDisplay.innerHTML = `Approximately <b>${p}%</b> chance of rain.`;
 }
 
+// Show something went wrong
 function DisplayError(message) {
+    resultDisplay.className = "";
+
     resultDisplay.innerHTML = "Dunno";
-    detailsDisplay.innerHTML = "Error: " + message;
+    detailsDisplay.innerHTML = "<b>Error:</b> " + message;
 }
 
 
-function Main() {
-    if ("geolocation" in navigator) {
-        resultDisplay.className = "loading";
-        resultDisplay.innerHTML = "Loading";
-        detailsDisplay.innerHTML = "Getting location"
+// When page loads, see if location is supported or not
+if ("geolocation" in navigator) {
+    // Get users location, on success this will make the API request
+    resultDisplay.className = "loading";
+    resultDisplay.innerHTML = "Loading";
+    detailsDisplay.innerHTML = "Getting location"
 
-        navigator.geolocation.getCurrentPosition(LocationSuccess, LocationError, locationOptions);
+    navigator.geolocation.getCurrentPosition(LocationSuccess, LocationError, locationOptions);
 
-    } else {
-        DisplayError("Your browser does not support getting location information.")
-    }
+} else {
+    DisplayError("Your browser does not support getting location information.")
 }
-
-Main();
-
